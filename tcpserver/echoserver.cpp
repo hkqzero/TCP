5917cb39ccc20bbb184d73c481976e47a6559504
kqhuang <hkqzero@126.com>
2016-06-28 00:29:12 -0400

    多进程版本

--- tcpserver/echoserver.cpp
+++ tcpserver/echoserver.cpp
@@ -9,6 +9,9 @@
 #include <cstdlib>
 #include <iostream>
 
+//处理链接
+int Work(int sockfd);
+
 int main()
 {
 	int server_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -31,37 +34,55 @@ int main()
 		return 0;
 	}
 	
-	char buf[10];
+	
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(client_addr);
+	pid_t childpid;
 	while (1) 
 	{
-		struct sockaddr_in client_addr;
-		int client_addr_len = sizeof(client_addr);
-		
 		printf("等待链接的到来\n");
-		int client_sock = accept( server_sock, (struct sockaddr*)&client_addr, (unsigned int *)&client_addr_len);
+		int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, (unsigned int *)&client_addr_len);
 
 		if (client_sock == -1) 
 		{
 			printf("accept error\n");
 			return 0;
 		}
+
 		printf("有新连接到来\n");
-		int n = recv(client_sock, buf, sizeof(buf), 0);
-		while (n > 0) 
+
+		//子进程
+		if((childpid = fork()) == 0)
 		{
-			printf("%s\n", buf);
-			if (send(client_sock, buf, sizeof(buf), 0) < 0) 
-			{
-				printf("send error\n");
-				return 0;
-			}
-			n = recv(client_sock, buf, sizeof(buf), 0);
+			close(server_sock);
+			Work(client_sock);
+
+			exit(0);
 		}
 
 		close(client_sock);
-		printf("\n\n连接离开\n");
 	}
 
 	close(server_sock);
 	return 0;
+}
+
+int Work(int sockfd)
+{
+	char buf[10];
+	int n = recv(sockfd, buf, sizeof(buf), 0);
+	while (n > 0) 
+	{
+		printf("%s", buf);
+		if (send(sockfd, buf, sizeof(buf), 0) < 0) 
+		{
+			printf("send error\n");
+			return 0;
+		}
+		n = recv(sockfd, buf, sizeof(buf), 0);
+	}
+	printf("\n");
+
+	return 0;
+
 }
